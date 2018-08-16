17903f2fa4009d56157e6e63b9f1dfb17233aa4f
yongtaiyu <376040925@qq.com>
2018-08-16 12:43:14 +0800

    init

---
+++ rocketmq/src/main/java/org/ydk/App.java
@@ -0,0 +1,15 @@
+package org.ydk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    
+}
