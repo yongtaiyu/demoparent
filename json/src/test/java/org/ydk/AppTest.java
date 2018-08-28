2ee0ee388356bd5cffc127162985c2c3aaa2ed6e
yongtaiyu <376040925@qq.com>
2018-08-28 15:38:58 +0800

    TRANS INIT

--- json/src/test/java/org/ydk/AppTest.java
+++ json/src/test/java/org/ydk/AppTest.java
@@ -7,8 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
@@ -18,14 +17,6 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
     }
 
     /**
